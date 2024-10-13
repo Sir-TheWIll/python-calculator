ecc3117c320c57aafb360f8c584da575008e2b61
Ande <andewillstuffambeno@gmail.com>
2024-10-13 17:20:22 +0100

    initial commit for python calculator

---
+++ simple-calculator.py
@@ -0,0 +1,21 @@
+#Python calculator
+
+
+operator = input("Enter an Operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is not a valid operator")
